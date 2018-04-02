c3100126e56a96ce7802522e5d23ebff4338775d
Nikita Popov <losnikitos@yandex-team.ru>
2018-04-03 00:20:43 +0300

    Auth comments

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,8 +8,19 @@ passport.use(new VKontakteStrategy({
         clientSecret: config.vk.appSecret,
         callbackURL: config.vk.callbackURL
     },
-    function myVerifyCallbackFn(accessToken, refreshToken, params, profile, done) {
-        const {id: vk_id, first_name, last_name, sex, screen_name, photo} = profile._json;
+    (accessToken, refreshToken, params, profile, done) => {
+
+        // Достаем поля из коллбека
+        const {
+            id: vk_id,
+            first_name,
+            last_name,
+            sex,
+            screen_name,
+            photo
+        } = profile._json;
+
+        // Добавляем пользователя или находим, если он уже заходил
         User.findOrCreate({where: {vk_id}, defaults: {first_name, last_name, photo}})
             .spread((user, created) => {
                 done(null, user);
@@ -17,18 +28,20 @@ passport.use(new VKontakteStrategy({
     }
 ));
 
+// Пользователь добавляется в базу в момент регистрации, поэтому тут ничего не делаем
 passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
+// Находим пользователя по ID
 passport.deserializeUser(function (id, done) {
     User.findById(id).then(user => done(null, user));
 });
 
 module.exports = {
-    callback: passport.authenticate('vkontakte', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    }),
-    login: passport.authenticate('vkontakte', {display: 'popup'})
+    // Редирект в user consent dialog
+    login: passport.authenticate('vkontakte', {display: 'popup'}),
+
+    // Коллбек из oauth.vk.com
+    callback: passport.authenticate('vkontakte', {successRedirect: '/', failureRedirect: '/login'})
 };
